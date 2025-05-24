a8b577fba4bf3555eefa71638c4f231096fdb4b6
yurie <yurie6312@gmail.com>
2025-05-24 14:12:35 +0900

    ポート重複の修正（app.listen を1つに）

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.static('public'));
