0666e9cfff2fecc742cb8539153adccf9f3ec6cc
yurie <yurie6312@gmail.com>
2025-05-24 14:07:45 +0900

    ポートを修正

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
